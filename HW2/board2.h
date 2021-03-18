746567348ced71242a89bbf9b441fb583f504d4f
Ben <shilinchen1010@gmail.com>
2021-03-17 21:01:21 -0700

    piece_move completed

--- HW2/board2.h
+++ HW2/board2.h
@@ -29,26 +29,20 @@ class Board {
         vector<Piece> get_all_piece(char color);
         vector<Board> get_all_moves(Piece piece);
 
-        //void piece_move(Piece selected_piece, Piece cur_piece, int row_dir, int col_dir, Board cur_board, vector<Board> &boards);
-        //void piece_jump(Piece piece, Board cur_board, int row, int col, vector<Piece> skipped, vector<Piece> path, vector<Board> &boards);
         void piece_jump(Piece piece, int row_dir, int col_dir, Board cur_board, vector<Board> &boards);
+        void piece_move(Piece piece, int row_dir, int col_dir, Board cur_board, vector<Board> &boards);
         bool valid(Piece piece, int row, int col);
-        //void piece_move(Piece selected_piece, int start_row, int stop_row, int row_dir, int col_dir, Board cur_board, vector<Piece> skipped, vector<Board> &boards);
-
-        vector<Board> dfs(Piece piece, int row, int col, int row_dir, int col_dir, Board cur_board, vector<Piece> skipped);
+    
 
         void init_board(); // init board informa tion
-        void get_piece_info(vector<vector<char> > board); // get piece info from given board.
+        void get_piece_info(); // get piece info from given board.
         void print_board();
-        void remove(vector<Piece> skipped);
-        void move(Piece start, Piece end);
         bool can_jump(Piece piece);
 
     private:
         bool could_jump(Piece piece, int row_dir, int col_dir, Board &cur_board);
         Piece get_piece(int row, int col);
-        void board_update(vector<vector<char> > &board, vector<Piece> skipped);
-        void set_new_board(Piece start, Piece end, vector<Piece> skipped);
+
 };
 
 vector<Board> Board::get_all_moves(Piece piece) {
@@ -66,22 +60,21 @@ vector<Board> Board::get_all_moves(Piece piece) {
         // king_jump/move
     }
     else if(piece.color == 'b') {
-        if(could_jump(piece, down, left, cur_board)) {
-            piece_jump(piece, down, left, cur_board, boards);
-        }
-        if(could_jump(piece, down, right, cur_board)) {
-            cout << "###" << endl;
-            piece_jump(piece, down, right, cur_board, boards);
-        }
+        if(cur_board.can_jump(piece)) piece_jump(piece, 1, 1, cur_board, boards);
+        piece_move(piece, 1, -1, cur_board, boards);
+        piece_move(piece, 1, 1, cur_board, boards);
     }
     else if(piece.color == 'w') {
         if(cur_board.can_jump(piece)) piece_jump(piece, -1, 1, cur_board, boards);
+        piece_move(piece, -1, -1, cur_board, boards);
+        piece_move(piece, -1, 1, cur_board, boards);
     }
     return boards;
 }
 
 void Board::piece_jump(Piece piece, int row_dir, int col_dir, Board cur_board, vector<Board> &boards) {
     if(!cur_board.can_jump(piece)) {
+        cur_board.get_piece_info();
         boards.push_back(cur_board);
         return;
     }
@@ -91,11 +84,12 @@ void Board::piece_jump(Piece piece, int row_dir, int col_dir, Board cur_board, v
         Piece new_piece(piece.row + 2*row_dir, piece.col + 2*col_dir, piece.color, false);
         Board copy = cur_board;
         copy.board[piece.row + row_dir][piece.col + col_dir] = '.';
-        swap(copy.board[piece.row][piece.col], copy.board[piece.row + 2*row_dir][piece.col + 2 * col_dir]);
+        copy.path.push_back({new_piece.row, new_piece.col});
+        swap(copy.board[piece.row][piece.col], copy.board[new_piece.row][new_piece.col]);
 
         // if reach end, make king
-        if((piece.row + 2 * row_dir == 0 || piece.row + 2 * row_dir == ROWS-1)) {
-            copy.board[piece.row + 2*row_dir][piece.col + 2 *col_dir] = toupper(copy.board[piece.row + 2*row_dir][piece.col + 2 *col_dir]);
+        if((new_piece.row == 0 || new_piece.row == ROWS-1)) {
+            copy.board[new_piece.row][new_piece.col] = toupper(copy.board[new_piece.row][new_piece.col]);
             boards.push_back(copy);
             return;
         }
@@ -107,19 +101,17 @@ void Board::piece_jump(Piece piece, int row_dir, int col_dir, Board cur_board, v
         Piece new_piece(piece.row + 2*row_dir, piece.col + 2*col_dir, piece.color, false);
         Board copy = cur_board;
         copy.board[piece.row + row_dir][piece.col + col_dir] = '.';
-        swap(copy.board[piece.row][piece.col], copy.board[piece.row + 2*row_dir][piece.col + 2 * col_dir]);
+        copy.path.push_back({new_piece.row, new_piece.col});
+        swap(copy.board[piece.row][piece.col], copy.board[new_piece.row][new_piece.col]);
 
         // if reach end, make king
-        if((piece.row + 2 * row_dir == 0 || piece.row + 2 * row_dir == ROWS-1)) {
-            copy.board[piece.row + 2*row_dir][piece.col + 2 *col_dir] = toupper(copy.board[piece.row + 2*row_dir][piece.col + 2 *col_dir]);
+        if((new_piece.row == 0 || new_piece.row == ROWS-1)) {
+            copy.board[new_piece.row][new_piece.col] = toupper(copy.board[new_piece.row][new_piece.col]);
             boards.push_back(copy);
             return;
         }
         piece_jump(new_piece, row_dir, col_dir, copy, boards);
     }
-        
-    
-     
 }
 
 bool Board::could_jump(Piece piece, int row_dir, int col_dir, Board &cur_board) {
@@ -146,42 +138,14 @@ bool Board::can_jump(Piece piece) {
     return false;
 }
 
-void Board::set_new_board(Piece start, Piece end, vector<Piece> skipped) {
-    this->move(start, end);
-    this->init_board();
-    this->get_piece_info(this->board);
-    this->remove(skipped);
-}
-
-void Board::move(Piece start, Piece end) {
-    
-    char tmp = this->board[start.row][start.col];
-    this->board[start.row][start.col] = this->board[end.row][end.col];
-    this->board[end.row][end.col] = tmp;
-
-    if(end.row == 0 || end.row == ROWS - 1) {
-        this->board[end.row][end.col] = toupper(this->board[end.row][end.col]);
-        if(end.color == 'w') {
-            this->white_king += 1;
-            this->white_left -= 1;
-        } else {
-            this->black_king += 1;
-            this->black_left -= 1;
-        }
-    }
-}
-
-void Board::remove(vector<Piece> skipped) {
-    for(Piece piece : skipped) {
-        this->board[piece.row][piece.col] = '.';
-        if(piece.color == 'w') {
-            this->white_left--;
-            if(piece.isKing) this->white_king --;
-        } 
-        else {
-            this->black_left--;
-            if(piece.isKing) this->black_king --;
-        }
+void Board::piece_move(Piece piece, int row_dir, int col_dir, Board cur_board, vector<Board> &boards) {
+    if(piece.row + row_dir < 0 || piece.row + row_dir >= ROWS || piece.col + col_dir < 0 || piece.col + col_dir >= COLS) return;
+    if(cur_board.board[piece.row+row_dir][piece.col+col_dir] == '.') {
+        Board copy = cur_board;
+        Piece new_piece(piece.row + row_dir, piece.col + col_dir, piece.color, false);
+        copy.path.push_back({new_piece.row, new_piece.col});
+        swap(copy.board[piece.row][piece.col], copy.board[new_piece.row][new_piece.col]);
+        boards.push_back(copy);
     }
 }
 
@@ -191,24 +155,24 @@ Piece Board::get_piece(int row, int col) {
     } else return Piece(row, col, this->board[row][col], false);
 }   
 
-void Board::get_piece_info(vector<vector<char> > board) {
-    this->board = board;
+void Board::get_piece_info() {
+    
     for(int row = 0; row < ROWS; row++) {
         for(int col = 0; col < COLS; col++) {
-            if(board[row][col] == 'w') {
+            if(this->board[row][col] == 'w') {
                 this->white_left++;
                 w_piece.push_back(Piece(row, col, 'w', false));
             }
-            else if(board[row][col] == 'W') {
+            else if(this->board[row][col] == 'W') {
                // this->white_left++;
                 this->white_king++;
                 w_piece.push_back(Piece(row, col, 'w', true));
             }
-            else if(board[row][col] == 'b') {
+            else if(this->board[row][col] == 'b') {
                 this->black_left++;
                 b_piece.push_back(Piece(row, col, 'b', false));
             }
-            else if(board[row][col] == 'B') {
+            else if(this->board[row][col] == 'B') {
                 //this->black_left++;
                 this->black_king++;
                 b_piece.push_back(Piece(row, col, 'b', true));

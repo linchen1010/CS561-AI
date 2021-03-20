6104f4e73fa9f576fbd79a865754f0bb9281c56c
Ben <shilinchen1010@gmail.com>
2021-03-19 18:40:22 -0700

    add clock and output

--- HW2/piece.h
+++ HW2/piece.h
@@ -15,16 +15,16 @@ class Piece {
     public:
         int row;
         int col;
-        string color;
+        char color;
         bool isKing = false;
-        Piece(int r, int c, string color, bool king) : row(r), col(c), color(color), isKing(king){}
-
-        //void move(int row, int col);
-        
-
+        Piece(int r, int c, char color, bool king) : row(r), col(c), color(color), isKing(king){}
+        void move(int row, int col);
 };
 
-// void Piece::move(int row, int col) {
-//     this->row = row;
-//     this->col = col;
-// }
+void Piece::move(int row, int col) {
+    this->row = row;
+    this->col = col;
+}
+
+
+

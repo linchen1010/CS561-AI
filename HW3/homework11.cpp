f6d46bda0148eeeb25b6e16631b1fc817f5b2cd2
Ben <shilinchen1010@gmail.com>
2021-04-21 22:22:14 -0700

    add store

--- HW3/homework11.cpp
+++ HW3/homework11.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
-#include <queue>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <unordered_map>
-#include <list>
 #include <math.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 #include "knowledgeBase.h"
 
 /* function declartion */
@@ -22,8 +24,8 @@ ifstream ifile;
 ofstream ofile;
 int numQuery;
 int numKB;
-vector<string> queries {};
-vector<string> queriesKB {};
+vector<string> queries{};
+vector<string> queriesKB{};
 
 /////////////////////////////
 
@@ -32,16 +34,10 @@ int main() {
     // checkInput();
     KnowledgeBase kb;
     // string str = "Start(x) & ~Ready(y) => ~play(x,y)";
-    // kb.getClause(str);
-    // kb.testClause(str);
-    for(auto str : queriesKB) {
-        kb.testClause(str);
-    }
-
+    kb.getKB(queriesKB);
+    kb.printCNF();
     return 0;
 }
-
-
 
 void input() {
     // read file content using ifstream
@@ -49,31 +45,28 @@ void input() {
     ifile >> numQuery;
     string line;
     int i = 0;
-    while(i++ < numQuery) {
+    while (i++ < numQuery) {
         ifile >> line;
         queries.push_back(line);
     }
     ifile >> numKB;
     i = 0;
-    while(getline(ifile, line) && i++ <= numKB) {
-        if(i == 1) continue; // skip new line
+    while (getline(ifile, line) && i++ <= numKB) {
+        if (i == 1) continue;  // skip new line
         queriesKB.push_back(line);
     }
     ifile.close();
 }
 
-void output() {
-    
-}
+void output() {}
 
 void checkInput() {
     cout << "Query: " << numQuery << endl;
-    for(string str: queries) {
+    for (string str : queries) {
         cout << str << endl;
     }
     cout << "KB: " << numKB << endl;
-    for(string str: queriesKB) {
+    for (string str : queriesKB) {
         cout << str << endl;
     }
-    
 }

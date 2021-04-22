65bafddbb99b8bd06720e88f24c16e75ef88bf2d
Ben <shilinchen1010@gmail.com>
2021-04-21 22:21:53 -0700

    add store

--- HW3/knowledgeBase.h
+++ HW3/knowledgeBase.h
@@ -1,76 +1,156 @@
-#include <queue>
 #include <algorithm>
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <stack>
+#include <queue>
 #include <set>
-#include "clause.h"
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
+using namespace std;
 
+/* Defining predicate */
+class Predicate {
+   public:
+    string name;
+    bool negative;
+    vector<string> args;  // could be variable or constant
+
+    void init();
+    bool isLiteral();
+};
+
+void Predicate::init() {
+    this->negative = false;
+    this->name = "";
+    this->args.clear();
+}
+
+bool isVariable(const string &arg) { return islower(arg[0]); }
+
+bool Predicate::isLiteral() {
+    for (int i = 0; i < this->args.size(); i++) {
+        if (isVariable(this->args[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/*****************************/
 
 class KnowledgeBase {
-    public: 
-        vector<Clause> getClause(string str);
-        bool ifAnd;
-        void testClause(string str);
+    public:
+        vector<vector<Predicate>> dataKB;  // contain all sentences (after transfering to CNF)
+
+        vector<Predicate> convertToCNF(string str, int &line);  // transfer FOL to CNF
+        void printCNF();
+        void getKB(vector<string> sentences);
+        void store(vector<Predicate> &sentence);
+        vector<pair<vector<Predicate>, int>> fetch(Predicate &p);
+
+   private:
+   
+    struct pos {  // entrance to our kb
+            vector<pair<int, int>> positive_literals;
+            vector<pair<int, int>> negative_literals;
+            vector<pair<int, int>> positive_sentences;
+            vector<pair<int, int>> negative_sentences;
+        };
+
+    unordered_map<string, pos> index;
 };
 /**
- * Take a string then generate the vector of clause of it
+ * Take a string then generate the vector of Predicate of it, also standarize
+ * the variable
  * @param str
- * @return vector of clause
-*/
-vector<Clause> KnowledgeBase::getClause(string str) {
-
-    vector<Clause> res;
-    Clause tmp;
+ * @param line -- standardized variable in argument
+ * @return vector of Predicate
+ */
+vector<Predicate> KnowledgeBase::convertToCNF(string str, int &line) {
+    vector<Predicate> res;
+    Predicate tmp;
     tmp.init();
     int startPos = 0;
-    
-    for(int i = 0; i < str.length(); i++) {
-        if(str[i] == '~') {
-            tmp.isFact = false;
-            startPos = i+1;
-        }
-        else if(str[i] == '(') {
-            tmp.predicate = str.substr(startPos, i-startPos);
-            startPos = i+1;
-        }
-        else if(str[i] == ',') {
-            tmp.args.push_back(Argument(str.substr(startPos, i-startPos)));
-            startPos = i+1;
-        }
-        else if(str[i] == ')') {
-            tmp.args.push_back(Argument(str.substr(startPos, i-startPos)));
-            startPos = i+1;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == '~') {
+            tmp.negative = true;
+            startPos = i + 1;
+        } else if (str[i] == '(') {
+            tmp.name = str.substr(startPos, i - startPos);
+            startPos = i + 1;
+        } else if (str[i] == ',') {
+            string arg = str.substr(startPos, i - startPos);
+            if (isVariable(arg)) arg += to_string(line);
+            tmp.args.push_back(arg);
+            startPos = i + 1;
+        } else if (str[i] == ')') {
+            string arg = str.substr(startPos, i - startPos);
+            if (isVariable(arg)) arg += to_string(line);
+            tmp.args.push_back(arg);
+            startPos = i + 1;
             res.push_back(tmp);
             tmp.init();
-        }
-        else if(str[i] == ' ' || str[i] == '&') {
-            startPos = i+1;
-        }
-        else if(str[i] == '=') {
-            // need to modify original one, so iterate by reference
-            for(Clause &clause : res) {
-                clause.isFact = !clause.isFact;
+        } else if (str[i] == ' ' || str[i] == '&') {
+            startPos = i + 1;
+        } else if (str[i] == '=') {
+            for (Predicate &p : res) {
+                p.negative = !p.negative;
             }
         }
     }
     return res;
 }
 
-/** 
- * Test kb string
- * @param str 
-*/
-void KnowledgeBase::testClause(string str) {
-    vector<Clause> res = getClause(str);
-    for(Clause clause : res) {
-        if(!clause.isFact) cout << "~";
-        cout << clause.predicate << " ";
-        for(auto arg : clause.args) {
-            cout << arg.arg << " ";
+void KnowledgeBase::getKB(vector<string> sentences) {
+    for (int i = 0; i < sentences.size(); i++) {
+        vector<Predicate> sentence = convertToCNF(sentences[i], i);
+        this->store(sentence);
+    }
+}
+
+/**
+ * store the given sentence (which is a vector of predicate) to the kb index
+ * @param sentence 
+ */
+void KnowledgeBase::store(vector<Predicate> &sentence) {
+    this->dataKB.push_back(sentence);
+    int ent = this->dataKB.size() - 1;
+    for (int i = 0; i < sentence.size(); i++) {
+        cout << sentence[i].name << " " << ent << " " << i << endl;
+        if (sentence[i].isLiteral()) {
+            if (!sentence[i].negative) {
+                index[sentence[i].name].positive_literals.push_back({ent, i});
+            } else {
+                index[sentence[i].name].negative_literals.push_back({ent, i});
+            }
+        } else {  // not literal
+            if (!sentence[i].negative) {
+                index[sentence[i].name].positive_sentences.push_back({ent, i});
+            } else {
+                index[sentence[i].name].negative_sentences.push_back({ent, i});
+            }
         }
     }
-    cout << "\n";
 }
+
+
+/**
+ * Test kb string
+ * @param str
+ */
+void KnowledgeBase::printCNF() {
+    int i = 0;
+    for (auto sentence : this->dataKB) {
+        cout << i++ << ": ";
+        for (auto Predicate : sentence) {
+            if (Predicate.negative) cout << "~";
+            cout << Predicate.name << " ";
+            for (auto arg : Predicate.args) {
+                cout << arg << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
